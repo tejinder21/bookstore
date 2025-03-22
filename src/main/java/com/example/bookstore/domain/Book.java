385bd56d2790560e087e088d56d8ef4c051607ea
tejinder21 <tejindekumar@myy.haaga-helia.fi>
2025-03-22 14:13:40 +0200

    ..

--- src/main/java/com/example/bookstore/domain/Book.java
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Use IDENTITY strategy
     private Long id;
 
     private String title;
